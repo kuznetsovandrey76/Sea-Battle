6ba7b4a1cf08a8d5d313e5bb9d06662b7f0b45e4
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-08-30 21:38:40 +0300

    popup done

--- main.js
+++ main.js
@@ -1,1 +1,28 @@
-console.log('Alright, alright, alright');
+let yourName = document.querySelector('.popup__your-name--input');
+let pcName = document.querySelector('.popup__pc-name--input');
+
+let yourNameButton = document.querySelector('.popup__your-name--button');
+let pcNameButton = document.querySelector('.popup__pc-name--button');
+
+let chooseFirstStepPlayer = document.querySelector('.popup__choose-first-move');
+
+yourNameButton.addEventListener('click', () => {
+	document.querySelector('.popup__your-name').classList.toggle('hidden');
+	document.querySelector('.popup__pc-name').classList.toggle('hidden');
+	console.log(`You: ${yourName.value}`);
+});
+
+pcNameButton.addEventListener('click', () => {
+	document.querySelector('.popup__pc-name').classList.toggle('hidden');
+	document.querySelector('.popup__choose-first-move').classList.toggle('hidden');
+	console.log(`Opponent: ${pcName.value}`);
+	document.querySelector('.popup__choose-you').value = yourName.value;
+	document.querySelector('.popup__choose-pc').value = pcName.value;
+});
+
+chooseFirstStepPlayer.addEventListener('click', (e) => {
+	if (e.target.value === yourName.value || e.target.value === pcName.value) {
+		console.log(`First step: ${e.target.value}`);
+		document.querySelector('.popup__choose-first-move').classList.toggle('hidden');
+	}
+});	
