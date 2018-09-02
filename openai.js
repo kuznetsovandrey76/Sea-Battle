7d618ad2b998c1e4415001cf36c648c48e7bf6a7
kuznetsovandrey76 <and.rey.q@yandex.ru>
2018-09-02 18:58:23 +0300

    shot 0.1

--- openai.js
+++ openai.js
@@ -6,7 +6,7 @@ let getRandom = function(n) {
 	return Math.floor(Math.random() * n);
 };
 
-// FIELDS
+// MATRIX
 
 // Создаем двумерный массив: rows - строки, columns - столбцы
 let create2dArray = function (rows, columns) {
@@ -17,31 +17,9 @@ let create2dArray = function (rows, columns) {
    return arr;
 }
 
-// Массив координат
-let matrix = create2dArray(10, 10); 
+// Матрицы для координат
+let matrixUser = create2dArray(10, 10); 
 let matrixPC = create2dArray(10, 10); 
-
-let field = getElement('.field');
-let dot = getElement('.dot');
-
-
-field.addEventListener('click', (e) => {
-	// 26px расстояние до внутреннего поля в playfield.png
-	if (e.offsetX - 26 > 2 && 
-		e.offsetX - 26 < 300 &&
-		e.offsetY - 26 > 2 &&
-		e.offsetY - 26 < 300) {
-		dot.classList.remove('hidden')
-		// Показывает координаты выстрела 
-		console.log(`x: ${Math.ceil((e.offsetX - 26) / 30)}, y: ${Math.ceil((e.offsetY - 26) / 30)}`);		
-		dot.style.left = 26 + 30 * Math.floor((e.offsetX - 26) / 30) + 'px';
-		dot.style.top = 26 + 30 * Math.floor((e.offsetY - 26) / 30) + 'px';
-		
-		// Изменяем матрицу координат
-		let coordX = Math.ceil((e.offsetX - 26) / 30);
-		let coordY = Math.ceil((e.offsetY - 26) / 30);
-	}
-});
 
 
 
@@ -99,7 +77,7 @@ let randomShip = function(type) {
 	}
 };
 
-let check = function(ship) {
+let check = function(ship, matrix) {
 	this.ship = ship;
 	// Проверка не заняты ли координаты другим кораблем или его обводкой (1 или 2)
 	for(let i = 0; i < this.ship.type; i++) {
@@ -125,7 +103,7 @@ let check = function(ship) {
 	return ship;
 };
 
-let addShip = function(ship) {
+let addShip = function(ship, matrix) {
 	// Прорисовка корабля и контура вокруг него
 	for(let i = 0; i < ship.type; i++) {
 		if (ship.direction == 1) {
@@ -190,7 +168,7 @@ let addShip = function(ship) {
 				matrix[ship.coordY - 1 + i][ship.coordX] = 2;	
 			}
 		}
-		// console.log(matrix)
+
 	}
 };
 
@@ -201,7 +179,7 @@ let createShip = function(func, type) {
 };
 
 
-let addAllShips = function() {
+let addAllShips = function(matrix, whoseShips) {
 	// Тип корабля и его количестов
 	let type = {
 		singledeck : 4,
@@ -215,28 +193,29 @@ let addAllShips = function() {
 	for(let i = 0; i < arr.length; i++ ) {
 	
 		// 1. Создаю рандомный корабль
-		let ship = check(createShip(randomShip, arr[i]));
+		let ship = check(createShip(randomShip, arr[i]), matrix);
 
 		// 2. Проверка, свободно ли место для размещения
 		while (!ship) {
-			ship = check(createShip(randomShip, arr[i]));
+			ship = check(createShip(randomShip, arr[i]), matrix);
 		}
 
 		// Информация о корабле
 		// console.log(ship);
 
 		// Добавление в массив, координат кораблей, для последующей прорисовки
-		userShips.push(ship);
+		whoseShips.push(ship);
 
 	 	// 3. Когда проверка пройдена, добавление коробля в матрицу вместе с обводкой
-		addShip(ship);	
+		addShip(ship, matrix);	
 	} 
 
 
 };
 
 // Формирование поля с рандомными кораблями
-addAllShips();
+addAllShips(matrixUser, userShips);
+addAllShips(matrixPC, pcShips);
 
 
 
@@ -252,7 +231,8 @@ let drawShips = function(arr, user) {
 		// Добавляем корабль на поле
 		let field = document.querySelector(user);
 		let div = document.createElement('div');
-		div.classList.add('ship');
+		// Скрываю корабли при добавлении на поле user'a
+		user == '.field-user' ? div.classList.add('ship', 'hidden') : div.classList.add('ship');
 		div.style.width = width + 'px';
 		div.style.height = height + 'px';
 		div.style.left = coordX + 'px';
@@ -263,4 +243,46 @@ let drawShips = function(arr, user) {
 }
 
 drawShips(userShips, '.field-user');
-drawShips(userShips, '.field-pc');
+drawShips(pcShips, '.field-pc');
+
+
+// SHOT
+
+let field = getElement('.field');
+
+field.addEventListener('click', (e) => {
+	// 26px расстояние до внутреннего поля в playfield.png
+	if (e.offsetX - 26 > 2 && 
+		e.offsetX - 26 < 300 &&
+		e.offsetY - 26 > 2 &&
+		e.offsetY - 26 < 300) {
+
+		// Показывает координаты выстрела 
+		// console.log(`x: ${Math.ceil((e.offsetX - 26) / 30)}, y: ${Math.ceil((e.offsetY - 26) / 30)}`);		
+		let coordX = Math.ceil((e.offsetX - 26) / 30);
+		let coordY = Math.ceil((e.offsetY - 26) / 30);
+
+		// Проверка куда произведен выстрел
+		// 0 - мимо
+		// 1 - попал
+		// 2 - мимо / обводка корабля
+		if (matrixUser[coordY - 1][coordX - 1] == 1) {
+			let field = document.querySelector('.field-user');
+			let div = document.createElement('div');
+			div.classList.add('knock');
+			div.style.left = 26 + 30 * Math.floor((e.offsetX - 26) / 30) + 'px';
+			div.style.top = 26 + 30 * Math.floor((e.offsetY - 26) / 30) + 'px';
+			field.appendChild(div); 
+			
+		} 
+		if (matrixUser[coordY - 1][coordX - 1] == 0 || matrixUser[coordY - 1][coordX - 1] == 2) {
+			let field = document.querySelector('.field-user');
+			let div = document.createElement('div');
+			div.classList.add('dot');
+			div.style.left = 26 + 30 * Math.floor((e.offsetX - 26) / 30) + 'px';
+			div.style.top = 26 + 30 * Math.floor((e.offsetY - 26) / 30) + 'px';
+			field.appendChild(div); 
+		}
+	}
+
+});
